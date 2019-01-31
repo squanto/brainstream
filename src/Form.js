f6d0415708117203742dc0626338ec7c5588a1ff
Hugo Melo <me@hugomelo.com>
2019-01-30 21:46:55 -0500

    downloads video

--- src/Form.js
+++ src/Form.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react"
+const electron = window.require("electron")
+
+const fs = electron.remote.require("fs")
+const ytdl = electron.remote.require("ytdl-core")
+
 
 class Form extends Component {
     constructor() {
         super()
         this.state = {
-            playlistURL: ""
+            videoURL: ""
         }
         this.handleInput = this.handleInput.bind(this)
         this.submit = this.submit.bind(this)
     }
 
     handleInput(event) {
-        this.setState({ playlistURL: event.target.value })
+        this.setState({ videoURL: event.target.value })
     }
 
     submit(event) {
         event.preventDefault()
         // send the stuff to download
         console.log("form submitted: ", this.state)
+
+        ytdl(this.state.videoURL)
+            .pipe(fs.createWriteStream('testvid.flv'))
+            .on('end', () => console.log("done downloading"))
     }
 
     render() {
         return (
             <form>
-                <input type="text" value={this.state.playlistURL} onChange={this.handleInput} placeholder="playlist url" />
+                <input type="text" value={this.state.videoURL} onChange={this.handleInput} placeholder="playlist url" />
                 <button onClick={this.submit}>Add</button>
             </form>
         )
